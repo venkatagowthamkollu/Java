e829c463d6a3aef2191de1826c77ab2754a11aa9
Venkata Gowtham Kollu <65944107+venkatagowthamkollu@users.noreply.github.com>
2021-12-29 15:30:53 +0530

    Create Operators.java

---
+++ Operators/Operators.java
@@ -0,0 +1,24 @@
+public class Operators{
+public static void main(String[] args)
+{
+int a=2,b=3,c=4;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+System.out.println(a%b);
+a+=1;
+b-=10;
+c%=20;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(a>b);
+System.out.println(bSystem.out.println(a==b);
+System.out.println(b!=c);
+System.out.println(ac);
+System.out.println(ac);
+System.out.println(ac);
+System.out.println(ac);
+}
+} 
