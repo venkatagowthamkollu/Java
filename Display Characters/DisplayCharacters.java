e26b44404f6c8e147f28e9f21d161ab81741579d
Venkata Gowtham Kollu <65944107+venkatagowthamkollu@users.noreply.github.com>
2021-12-29 15:34:39 +0530

    Create DisplayCharacters.java

---
+++ Display Characters/DisplayCharacters.java
@@ -0,0 +1,14 @@
+import java.util.*; 
+public class DisplayCharacters
+{ 
+        public static void main(String[] args)
+ { 
+        Scanner sc=new Scanner(System.in); 
+        System.out.println("Enter the digits:"); 
+        for (int i = 0; i < 4; ++i)
+        { 
+            int n=sc.nextInt(); 
+            System.out.println(n + "-" + (char)n); 
+        } 
+  } 
+}           
